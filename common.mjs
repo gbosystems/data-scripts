0c92bb9eaf0624fe2de38e01a8b40fd264303ede
Geoff O'Donnell <odonne89@outlook.com>
2024-03-07 20:14:29 -0500

    Adds download-ports script

---
+++ common.mjs
@@ -0,0 +1,69 @@
+/**
+ * Copyright (c) 2024 GBO Systems
+ */
+
+
+import * as fs from "fs"
+
+
+export const ensurePathExistsSync = async (path) => {
+
+    if (!fs.existsSync(path)) {
+        fs.mkdirSync(path, { recursive: true });
+    }
+}
+
+export const getArgs = () => {
+
+    const args = process.argv.slice(2);
+    const result = {};
+
+    for (let i = 0; i < args.length; i++) {
+        if (args[i].startsWith("--")) {
+            let key = args[i].substr(2).toLowerCase();
+
+            if (args[i + 1]) {
+                if (args[i + 1].startsWith("--")) {
+                    result[key] = true;
+                } else {
+                    result[key] = args[i + 1];
+                    i++;
+                }
+            } else {
+                result[key] = true;
+            }
+        }
+    }
+
+    return result;
+}
+
+export const groupBy = (array, selector) => {
+
+    const result = {};
+
+    if (Array.isArray(array)) {
+        for (let i = 0; i < array.length; i++) {
+
+            const item = array[i];
+            const key = selector(item, i);
+            const group = result[key] ? result[key] : (result[key] = []);
+
+            group.push(item);
+        }
+    }
+
+    return result;
+}
+
+export const writeObjectToFile = async (fileName, data) => {
+
+    return new Promise((resolve, reject) => {
+        fs.writeFile(fileName, JSON.stringify(data), (err) => {
+            if (err) {
+                reject(err);
+            }
+            resolve();
+        });
+    });
+}
---
+++ download-ports.mjs
@@ -0,0 +1,118 @@
+/**
+ * Copyright (c) 2024 GBO Systems
+ *
+ * Download ports from the NGA and store as GeoJSON files
+ * 
+ * https://public.cyber.mil/pki-pke/tools-configuration-files/
+ *  -> DoD Approved External PKI Certificate Trust Chains - Version 10.2
+ *      -> _DoD\Trust_Anchors_Self-Signed\0-DoD_Root_CA_3.cer
+ *      -> _DoD\Intermediate_and_Issuing_CA_Certs\1-DOD_SW_CA-66.cer
+ * Combine certs into single .pem file
+ * Powershell: $env:NODE_EXTRA_CA_CERTS=".\cert-chain.pem"
+ * Ubuntu?: https://askubuntu.com/a/1061060
+ */
+
+
+import { ensurePathExistsSync, getArgs, groupBy, writeObjectToFile } from "./common.mjs"
+import * as path from "path"
+
+
+const url = "https://msi.pub.kubic.nga.mil/api/publications/world-port-index?output=json";
+const now = new Date();
+const accuracy = 6;
+
+
+const toGeoJsonFeature = (port) => {
+
+    const id = port.globalId.substr(1, port.globalId.length - 2);
+    const lat = parseFloat(port.ycoord.toFixed(accuracy));
+    const lon = parseFloat(port.xcoord.toFixed(accuracy));
+
+    return {
+        id: id,
+        type: 'Feature',
+        properties: port,
+        geometry: {
+            type: 'Point',
+            coordinates: [lon, lat]
+        }
+    }
+}
+
+const execute = async () => {
+
+    const args = getArgs();
+    
+    await ensurePathExistsSync(args.path);
+    await ensurePathExistsSync(path.join(args.path, "country"));
+    await ensurePathExistsSync(path.join(args.path, "harbor-size"));
+    await ensurePathExistsSync(path.join(args.path, "harbor-type"));
+
+
+    const data = await fetch(url).then(r => r.json());
+    const allFeatures = data.ports.map(toGeoJsonFeature).sort((a, b) => {
+        const aValue = a.portName ? a.portName.toLowerCase() : "";
+        const bValue = b.portName ? b.portName.toLowerCase() : "";
+
+        return aValue <= bValue ? -1 : 1;
+    });
+
+    await writeObjectToFile(path.join(args.path, "all.geojson"), {
+        type: "FeatureCollection",
+        features: allFeatures
+    });   
+
+    const byHarborSize = groupBy(
+        allFeatures.filter(s => typeof s.properties.harborSize === "string"),
+        s => s.properties.harborSize);
+
+    for (let key in byHarborSize) {
+        await writeObjectToFile(path.join(args.path, "harbor-size", `${key.toLowerCase()}.geojson`), {
+            type: "FeatureCollection",
+            features: byHarborSize[key]
+        });
+    }
+
+    const byHarborType = groupBy(
+        allFeatures.filter(s => typeof s.properties.harborType === "string"),
+        s => s.properties.harborType);
+    
+    for (let key in byHarborType) {
+        await writeObjectToFile(path.join(args.path, "harbor-type", `${key.toLowerCase()}.geojson`), {
+            type: "FeatureCollection",
+            features: byHarborType[key]
+        });
+    }
+
+    const byCountryCode = groupBy(
+        allFeatures.filter(s => typeof s.properties.countryCode === "string"),
+        s => s.properties.countryCode);
+    
+    for (let key in byCountryCode) {
+        await writeObjectToFile(path.join(args.path, "country", `${key.toLowerCase()}.geojson`), {
+            type: "FeatureCollection",
+            features: byCountryCode[key]
+        });
+    }
+
+    await writeObjectToFile(path.join(args.path, "metadata.json"), {
+        source: url, 
+        updated: now.getTime(),
+        total: allFeatures.length,
+        query: {
+            "country": Object.keys(byCountryCode),
+            "harbor-size": Object.keys(byHarborSize),
+            "harbor-type": Object.keys(byHarborType),
+        }
+    });
+}
+
+try {
+    await execute();
+} catch (ex) {
+    console.error(ex);
+    process.exit(1);
+}
+
+console.log("Downloaded ports.");
+process.exit(0);
